58a3bc17c56a217f59721a6bb4e812050d45a5fb
LAPTOP-UAONIT64\admin <1033729276@qq.com>
2021-08-16 14:51:08 +0800

    fix:优化配置

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,40 @@
 const path = require('path')
-// const webpack = require('webpack')
-// const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+// dll
+const webpack = require('webpack')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
 // 分析
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
-const isProduction = process.env.NODE_ENV === 'production' ? true : false
+const isProduction = process.env.NODE_ENV === 'production'
 // 代码压缩 项目变大了。
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 // gzip 低版本
 const CompressionPlugin = require('compression-webpack-plugin')
+// 进度条
+const WebpackBar = require('webpackbar');
+// 多线程
+const Happypack = require('happypack');
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-
+// cdn
+const cdn = {
+  css: [],
+  js: [
+      'https://cdn.bootcss.com/vue/2.6.11/vue.runtime.min.js',
+      'https://cdn.bootcss.com/vue-router/3.2.0/vue-router.min.js',
+      'https://cdn.bootcss.com/vuex/3.4.0/vuex.min.js',
+      'https://cdn.bootcss.com/axios/0.21.1/axios.min.js',
+  ]
+}
 module.exports = {
   publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
-  // productionSourceMap: false,
+  productionSourceMap: true,
   devServer: {
     host: 'localhost',
     port: 8078,
-    // before(app, server) {
-    //   app.get(/.*.(js)$/, (req, res, next) => {
-    //     req.url = req.url + '.gz'
-    //     res.set('Content-Encoding', 'gzip')
-    //     next()
-    //   })
-    // }
   },
   css: {
     extract: false
@@ -37,9 +44,27 @@ module.exports = {
       .set('@', resolve('src'))
       .set('@components', resolve('src/components'))
       .set('@static', resolve('src/static'))
+    // 生产环境配置
+    if (isProduction) {
+      // 生产环境注入cdn
+      config.plugin('html')
+          .tap(args => {
+              args[0].cdn = cdn;
+              return args;
+          });
+    }
   },
   configureWebpack: config => {
-    config.externals = { AMap: 'AMap' }
+    config.externals = { 
+      AMap: 'AMap',
+      vue: 'Vue',
+      vuex: 'Vuex',
+      'vue-router': 'VueRouter',
+      axios: 'axios'
+    }
+    config.plugins.push(
+      new WebpackBar(),
+    )
     config.plugins.push(          
       new CompressionPlugin({
         filename: "[path][base].gz",
@@ -48,6 +73,23 @@ module.exports = {
         threshold: 10240,
         minRatio: 0.8,
       })
+    )
+    config.plugins.push(
+      new webpack.DllReferencePlugin({
+				context: process.cwd(),
+				manifest: require('./public/vendor/vendor-manifest.json')
+			})
+    )
+    config.plugins.push(
+			// 将 dll 注入到 生成的 html 模板中
+			new AddAssetHtmlPlugin({
+				// dll文件位置
+				filepath: path.resolve(__dirname, './public/vendor/*.js'),
+				// dll 引用路径
+				publicPath: './vendor',
+				// dll最终输出的目录
+				outputPath: './vendor'
+			}),
     )
     config.plugins.push(
       new BundleAnalyzerPlugin({
@@ -63,7 +105,13 @@ module.exports = {
         logLevel: 'info'
       })
     )
-
+    config.plugins.push(
+      new Happypack({
+        loaders: ['babel-loader', 'vue-loader', 'url-loader'],
+        cache: true,
+        threads: 5 // 线程数取决于你电脑性能的好坏，好的电脑建议开更多线程
+      })
+    )
     // 生产环境相关配置
     if (isProduction) {
       // 代码压缩
@@ -78,7 +126,7 @@ module.exports = {
               pure_funcs: ['console.log']
             }
           },
-          sourceMap: false,
+          sourceMap: true,
           parallel: true
         })
       )
