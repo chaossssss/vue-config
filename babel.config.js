420312b95c26ceb8b9ddea97c272024c65085ffd
LAPTOP-UAONIT64\admin <1033729276@qq.com>
2021-08-23 10:18:54 +0800

    fix:优化配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  // 按需引入
+  // plugins: [
+  //   // element官方教程
+  //   [
+  //     "component",
+  //     {
+  //       libraryName: "element-ui",
+  //       styleLibraryName: "theme-chalk"
+  //     }
+  //   ]
+  // ]
 }
